a5d72a5af940075daea53b78b884ef9932cb9509
Charles <Charles@DESKTOP-8AP1QJH.frontier.com>
2016-12-23 23:42:47 -0500

    Update to MapDatabase and Tile classes

--- src/javagame/MapDatabase.java
+++ src/javagame/MapDatabase.java
@@ -17,7 +17,10 @@ public class MapDatabase {
 	public MapDatabase() throws SlickException{
 		
 		//Tiles
-		b = new Tile("res/objects/BlackBox.png");
+		b = new Tile("res/objects/BlackBox.png","black", 100, 100);
+		w = new Tile("res/objects/WhiteBox.png","white", 100, 100);
+		r = new Tile("res/objects/RedBox.png","red", 100, 100);
+		g = new Tile("res/objects/GrayBox.png","gray", 100, 100);
 
 		Tile[][] Map1 = {
 				{b,b,b,b,b,b},
--- src/javagame/Tile.java
+++ src/javagame/Tile.java
@@ -5,19 +5,24 @@ import org.newdawn.slick.SlickException;
 
 public class Tile extends Image{
 	
+	//Name
+	String _name;
 	//Location
 	int _X;
 	int _Y;
-	
 	//Size
 	int _sizeX;
 	int _sizeY;
 
-	public Tile(java.lang.String str, int w, int h) throws SlickException{
+	public Tile(java.lang.String str, String name, int w, int h) throws SlickException{
 		super(str);
 		_sizeX = w;
 		_sizeY = h;
 		
+	}
+	
+	public String getName(){
+		return _name;
 	}
 	
 	public void setLocation(int x, int y){
@@ -29,4 +34,15 @@ public class Tile extends Image{
 		_X = x;
 	}
 	
+	public void setY(int y){
+		_Y = y;
+	}
+	
+	public int getX(){
+		return _X;
+	}
+	
+	public int getY(){
+		return _Y;
+	}
 }
